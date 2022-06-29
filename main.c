8879249e243cb7d703c62b648ab97c5db616be05
Gaurav Raj <techw803@gmail.com>
2022-06-29 22:23:51 +0530

    Added certain key mappings

--- main.c
+++ main.c
@@ -9,8 +9,20 @@
 #include <unistd.h>
 
 /*** defines ***/
-#define GVIM_VERSION "0.0.1"
+#define GVIM_VERSION "0.1.5"
 #define CTRL_KEY(k) ((k) & 0X1f)
+
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
+  PAGE_UP,
+  PAGE_DOWN
+};
 
 /*** data ***/
 struct editorConfig {
@@ -56,7 +68,7 @@ void enableRawMode() {
   };
 }
 
-char editorReadKey() {
+int editorReadKey() {
   int nread;
   char c;
   while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
@@ -64,7 +76,62 @@ char editorReadKey() {
       die("read");
     }
   }
-  return c;
+
+  if (c == '\x1b') {
+    char seq[3];
+
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) {
+      return '\x1b';
+    }
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) {
+      return '\x1b';
+    }
+
+    if (seq[0] == '[') {
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) {
+          return '\x1b';
+        }
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+            case '1':
+              return HOME_KEY;
+            case '3':
+              return DEL_KEY;
+            case '4':
+              return END_KEY;
+            case '5':
+              return PAGE_UP;
+            case '6':
+              return PAGE_DOWN;
+            case '7':
+              return HOME_KEY;
+            case '8':
+              return END_KEY;
+          }
+        }
+      } else {
+          switch (seq[1]) {
+            case 'A':
+              return ARROW_UP;
+            case 'B':
+              return ARROW_DOWN;
+            case 'C':
+              return ARROW_RIGHT;
+            case 'D':
+              return ARROW_LEFT;
+            case 'H':
+              return HOME_KEY;
+            case 'F':
+              return END_KEY;
+          }
+        }
+    }
+
+    return '\x1b';
+  } else {
+    return c;
+  }
 }
 
 int getCursorPosition(int *row, int *cols) {
@@ -215,25 +282,33 @@ void editorRefreshScreen() {
 }
 
 /*** input ***/
-void editorMoveCursor(char key) {
+void editorMoveCursor(int key) {
   switch (key) {
-    case 'a':
-      E.cx--;
+    case ARROW_LEFT:
+      if (E.cx != 0) {
+        E.cx--;
+      }
       break;
-    case 'd':
-      E.cx++;
+    case ARROW_RIGHT:
+      if (E.cx != E.screencols - 1) {
+        E.cx++;
+      }
       break;
-    case 'w':
-      E.cy--;
+    case ARROW_UP:
+      if (E.cy != 0) {
+        E.cy--;
+      }
       break;
-    case 's':
-      E.cy++;
+    case ARROW_DOWN:
+      if (E.cy != E.screenrows - 1) {
+        E.cy++;
+      }
       break;
   }
 }
 
 void editorProcessKeypress() {
-  char c = editorReadKey();
+  int c = editorReadKey();
 
   switch (c) {
     case CTRL_KEY('q'):
@@ -241,10 +316,24 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
-    case 'w':
-    case 's':
-    case 'a':
-    case 'd':
+    case HOME_KEY:
+      E.cx = 0;
+      break;
+    case END_KEY:
+      E.cx = E.screencols - 1;
+      break;
+    case PAGE_UP:
+    case PAGE_DOWN:
+      {
+        int times = E.screenrows;
+        while (times--) {
+          editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+        }
+      }
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
       editorMoveCursor(c);
       break;
   }
