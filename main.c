961d8cb3e701999f7c3448d86aa2bae04fa4a9de
Gaurav Raj <techw803@gmail.com>
2022-06-30 13:25:46 +0530

    Fixed backspace bug and added enter key support, text editor is almost finished

--- main.c
+++ main.c
@@ -18,7 +18,7 @@
 #include <time.h>
 
 /*** defines ***/
-#define GVIM_VERSION "0.4.2"
+#define GVIM_VERSION "0.4.8"
 #define GVIM_TAB_STOP 8
 #define GVIM_QUIT_TIMES 2
 #define CTRL_KEY(k) ((k)&0X1f)
@@ -263,10 +263,14 @@ void editorUpdateRow(erow *row) {
   row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len) {
-  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+void editorInsertRow(int at, char *s, size_t len) {
+  if (at < 0 || at > E.numrows) {
+    return;
+  }
 
-  int at = E.numrows;
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
@@ -329,10 +333,25 @@ void editorRowDelChar(erow *row, int at) {
 /*** editor operations ***/
 void editorInsertChar(int c) {
   if (E.cy == E.numrows) {
-    editorAppendRow("", 0);
+    editorInsertRow(E.numrows, "", 0);
   }
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
+}
+
+void editorInsertNewline() {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  } else {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
 }
 
 void editorDelChar() {
@@ -392,7 +411,7 @@ void editorOpen(char *filename) {
     while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) {
       linelen--;
     }
-    editorAppendRow(line, linelen);
+    editorInsertRow(E.numrows, line, linelen);
   }
   free(line);
   fclose(fp);
@@ -423,11 +442,6 @@ void editorSave() {
 
   free(buf);
   editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
-
-//  ftruncate(fd, len);
-//  write(fd, buf, len);
-//  close(fd);
-//  free(buf);
 }
 
 /*** append buffer ***/
@@ -628,7 +642,7 @@ void editorProcessKeypress() {
 
   switch (c) {
     case '\r':
-      /* Todo */
+      editorInsertNewline();
       break;
 
     case CTRL_KEY('q'):
@@ -662,8 +676,8 @@ void editorProcessKeypress() {
     case DEL_KEY:
       if (c == DEL_KEY) {
         editorMoveCursor(ARROW_RIGHT);
-        editorDelChar();
       }
+      editorDelChar();
       break;
 
     case PAGE_UP:
